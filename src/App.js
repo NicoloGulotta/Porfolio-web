2083792c876a0c95ac62cce4e924bd7be09c6251
Nicolò Gulotta <gulottanico1998@gmail.com>
2024-08-27 21:11:50 +0200

    save

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar.jsx';
 // import Jumbotron from './components/Jumbotron/Jumbotron.jsx';
-// import Content from './components/Content/Content.jsx'
+import Content from './components/Content/Content.jsx'
 function App() {
   return (
     <div className='container'>
       <Sidebar />
       {/* <Jumbotron /> */}
-      {/* <Content /> */}
+      <Content />
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Container, Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import './Sidebar.css';
 import name from '../../assets/name-big.jpg'
 import logobig from '../../assets/logo-small.png'
@@ -7,34 +7,29 @@ import logobig from '../../assets/logo-small.png'
 const Sidebar = () => {
   return (
     <Navbar className="sidebar">
-      <Container className=''>
-        <Navbar.Brand href="/" className='image-box'>
-          <img src={name} alt="My Name" className="name-image" />
-          {/* <img src={logobig} alt="Big logo" className="jumbotron-image" /> */}
-        </Navbar.Brand>
-        <td>
-          <Navbar.Collapse id="basic-sidebar-nav">
-            <Nav>
-              <Nav.Link href="/#">
-                <img src={logobig} alt="Big logo" className="jumbotron-image" />
-                CHI SONO
-              </Nav.Link>
-              <Nav.Link href="/#">
-                <img src={logobig} alt="Big logo" className="jumbotron-image" />
-                COSA FACCIO
-              </Nav.Link>
-              <Nav.Link href="/#">
-                <img src={logobig} alt="Big logo" className="jumbotron-image" />
-                COME LO FACCIO
-              </Nav.Link>
-              <Nav.Link href="/#">
-                <img src={logobig} alt="Big logo" className="jumbotron-image" />
-                CHIEDI DI PIU'
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </td>
-      </Container>
+
+      <Navbar.Brand href="/" className='image-box'>
+        <img src={name} alt="My Name" className="name-image" />
+      </Navbar.Brand>
+
+      <Navbar.Collapse id="basic-sidebar-nav" className='link-box'>
+        <Nav.Link href="/#">
+          <img src={logobig} alt="Big logo" className="jumbotron-image" />
+          CHI SONO
+        </Nav.Link>
+        <Nav.Link href="/#">
+          <img src={logobig} alt="Big logo" className="jumbotron-image" />
+          COSA FACCIO
+        </Nav.Link>
+        <Nav.Link href="/#">
+          <img src={logobig} alt="Big logo" className="jumbotron-image" />
+          COME LO FACCIO
+        </Nav.Link>
+        <Nav.Link href="/#">
+          <img src={logobig} alt="Big logo" className="jumbotron-image" />
+          CHIEDI DI PIU'
+        </Nav.Link>
+      </Navbar.Collapse>
     </Navbar >
   );
 };
