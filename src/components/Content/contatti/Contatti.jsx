3e3998e5640278aa8d7084321840834422220e3b
Nicolò Gulotta <gulottanico1998@gmail.com>
2024-09-03 19:54:56 +0200

    Save

--- src/components/Content/contatti/Contatti.jsx
+++ src/components/Content/contatti/Contatti.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import './Contatti.css';
 import { Modal, Button } from 'react-bootstrap';
 
@@ -11,30 +11,45 @@ export default function Contatti() {
 
     return (
         <div className='container-con'>
-            <h1 className='text-light'>Contattami</h1>
-            <ul>
+            <h2 className='text-light'>Contatti</h2>
+            <ul className='social-icons'>
                 <li>
-                    <a className='info' href="https://www.linkedin.com/in/luca-giordano-1">
+                    <a className='info' href="https://www.linkedin.com/in/gulotta13">
                         <FontAwesomeIcon icon={faLinkedin} className='icon' /> LinkedIn
                     </a>
                 </li>
                 <li>
-                    <a className='info' href="https://github.com/LucaGiordano97">
+                    <a className='info' href="https://github.com/NicoloGulotta">
                         <FontAwesomeIcon icon={faGithub} className='icon' /> GitHub
                     </a>
                 </li>
-                <Button variant="outline-light" onClick={handleShow}>
-                    Contattaci
-                </Button>
+                <li>
+                    <a className='info' href="https://www.facebook.com/Nik.Gulotta.98/?locale=it_IT">
+                        <FontAwesomeIcon icon={faFacebook} className='icon' /> Facebook
+                    </a>
+                </li>
+                <li>
+                    <a className='info' href="https://twitter.com/your-username">
+                        <FontAwesomeIcon icon={faInstagram} className='icon' /> Instagram
+                    </a>
+                </li>
+                <li>
+                    <a className='info' href="https://twitter.com/your-username">
+                        <FontAwesomeIcon icon={faInstagram} className='icon' /> Instagram
+                    </a>
+                </li>
             </ul>
+            <Button variant="outline-dark" onClick={handleShow}>
+                Contattami
+            </Button>
             <Modal show={showModal} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Contattaci</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <p className='text-dark'>
-                        Puoi contattarci al numero di telefono 123-456-7890 o
-                        all'indirizzo email info@hairsalon.com.
+                        Email:
+                        <a className='text-dark' href="gulottanico1998@gmail.com">gulottanico1998@gmail.com</a>
                     </p>
                 </Modal.Body>
                 <Modal.Footer>
--- src/components/Content/profilo/Profilo.jsx
+++ src/components/Content/profilo/Profilo.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import './Profilo.css'
-import foto from './profilo.jpg'
-export default function Profilo() {
+import React from 'react';
+import foto from './profilo.jpg';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Profilo.css';
+
+const Profilo = () => {
     return (
-        <div className='container-profile'>
-            <div className='image-part'>
-                <img src={foto} alt="Nicolò Gulotta" className="profile-image" />
-            </div>
-            <div className="text-part">
-                <span>WEB DEVELOPER FULL-STACKS <strong>M.E.R.N.</strong> </span>
-                <p>Professionista versatile con esperienza nella
-                    ristorazione, nella logistica e una forte passione per lo sviluppo
-                    web. Orientato al cliente, con ottime
-                    capacità comunicative, di problem-solving e di lavoro
-                    in team. Esperienza nella gestione di magazzino, organizzazione del
-                    lavoro e coordinamento di team. In cerca di nuove sfide
-                    professionali nel settore dello sviluppo web, mettendo a frutto le
-                    competenze
-                    tecniche acquisite e la passione per l'apprendimento continuo.</p>
-            </div>
-        </div>
-    )
-}
+        <Container className="profilo">
+            <Row>
+                <Col md={4}>
+                    <img src={foto} alt="Nicolò Gulotta" className="profile-image" />
+                </Col>
+                <Col md={8}>
+                    <div className="text-part">
+                        <span>WEB DEVELOPER FULL-STACKS <strong>M.E.R.N.</strong> </span>
+                        <p>Professionista versatile con esperienza nella
+                            ristorazione, nella logistica e una forte passione per lo sviluppo
+                            web. Orientato al cliente, con ottime
+                            capacità comunicative, di problem-solving e di lavoro
+                            in team. Esperienza nella gestione di magazzino, organizzazione del
+                            lavoro e coordinamento di team. In cerca di nuove sfide
+                            professionali nel settore dello sviluppo web, mettendo a frutto le
+                            competenze
+                            tecniche acquisite e la passione per l'apprendimento continuo.</p>
+                    </div></Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Profilo;
