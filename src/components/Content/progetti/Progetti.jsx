3b41629c790cbb65f898795dc5cb06ff257378cd
Nicolò Gulotta <gulottanico1998@gmail.com>
2024-09-09 19:51:10 +0200

    Save

--- src/components/Content/progetti/Progetti.jsx
+++ src/components/Content/progetti/Progetti.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel from 'react-bootstrap/Carousel';
-import './Progetti.css';
-import Capstone from '../../../assets/Screenshot-Capstone.png';
-import LinkSmart from '../../../assets/Screenshot BarberLink-Smartphone.jpg';
-import ComingSoon from '../../../assets/images.png';
-import TodoList from '../../../assets/Screenshot 2024-09-05 211849.png';
+import './Progetti.css'
+import Capstone from '../../../assets/Screenshot-Capstone.png'
+import LinkSmart from '../../../assets/Screenshot BarberLink-Smartphone.jpg'
+import ComingSoon from '../../../assets/images.png'
+import TodoList from '../../../assets/Screenshot 2024-09-05 211849.png'
 
 export default function Progetti() {
     return (
-        <Carousel className='container-prog'>
-            <Carousel.Item>
-                <img className="image" src={Capstone} alt="Screenshot Capstone" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className="image" src={LinkSmart} alt="Screenshot BarberLink" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className="image" src={TodoList} alt="TodoList" />
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className="image" src={ComingSoon} alt="Coming Soon" />
-            </Carousel.Item>
-        </Carousel>
+        <div className='container-pro'>
+            <h2 className='text-light'>Progetti</h2>
+            <Carousel indicators={false} controls={false} fade={false} pause={false}>
+                <Carousel.Item interval={3000}>
+                    <img className='image' src={Capstone} alt="Screenshot Capstone" />
+                </Carousel.Item>
+
+                <Carousel.Item interval={3000}>
+                    <img className='image' src={LinkSmart} alt="Screenshot BarberLink" />
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img className='image' src={TodoList} alt="TodoList" />
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img className='image' src={ComingSoon} alt="Coming Soon" />
+                </Carousel.Item>
+            </Carousel>
+        </div >
     );
 }
-
