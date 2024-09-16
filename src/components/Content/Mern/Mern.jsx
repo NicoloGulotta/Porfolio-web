dea33f66c30327ec9accadd04aabaa2b9b3cd9ab
Nicolò Gulotta <gulottanico1998@gmail.com>
2024-09-16 19:37:44 +0200

    Save

--- src/components/Content/Mern/Mern.jsx
+++ src/components/Content/Mern/Mern.jsx
@@ -3,68 +3,69 @@ import mern from '../../../assets/Mern-stack.png';
 import './Mern.css';
 import { Button } from 'react-bootstrap';
 
-
 export default function MernStack() {
     const [showDetails, setShowDetails] = useState(false);
 
     const toggleDetails = () => {
-        setShowDetails(!showDetails);
+        setShowDetails(prevState => !prevState);
     };
 
     return (
         <div className='container-mern mb-4'>
             <h1 className='title'>Work-Flow dello stack M.E.R.N.</h1>
-            <img className='pic' src={mern} alt="mern-pic" />
-            <Button variant="outline-light" className=" mt-3" onClick={toggleDetails}>
+            <img className='pic' src={mern} alt="Diagramma dello stack M.E.R.N." />
+            <Button variant="outline-light" className="mt-3" onClick={toggleDetails}>
                 {showDetails ? 'Nascondi Dettagli' : 'Mostra Dettagli'}
             </Button>
 
             {showDetails && (
-                <ol className="text-white mt-3">
-                    <li>
-                        <strong>HTML, CSS & JS:</strong>
-                        <ul>
-                            <li><strong>HTML:</strong> Struttura semantica delle pagine web.</li>
-                            <li><strong>CSS:</strong> Stile e layout delle pagine, adattamento responsivo.</li>
-                            <li><strong>JavaScript:</strong> Interattività e dinamismo sul lato client.</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <strong>React:</strong>
-                        <ul>
-                            <li>Libreria JavaScript per la costruzione di interfacce utente dinamiche e componenti riutilizzabili.</li>
-                            <li>Gestione dello stato dell'applicazione con facilità, attraverso componenti e hook.</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <strong>Node:</strong>
-                        <ul>
-                            <li>Ambiente di runtime JavaScript lato server, che permette l'esecuzione di JavaScript al di fuori del browser.</li>
-                            <li>Creazione di applicazioni server-side con efficienza e scalabilità.</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <strong>Express:</strong>
-                        <ul>
-                            <li>Framework minimalista per Node.js, utilizzato per creare API e gestire routing e middleware.</li>
-                            <li>Semplificazione della gestione delle richieste HTTP, autenticazione, e gestione delle sessioni.</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <strong>MongoDB:</strong>
-                        <ul>
-                            <li>Database NoSQL orientato ai documenti, che consente di memorizzare dati in formato JSON-like.</li>
-                            <li>Flessibilità nella gestione di dati non strutturati e scalabilità per grandi volumi di dati.</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <strong>Connecting & Deploying:</strong>
-                        <ul>
-                            <li><strong>Connecting:</strong> Integrazione del front-end e back-end, assicurando un flusso di dati senza interruzioni tra server e client.</li>
-                            <li><strong>Deploying:</strong> Distribuzione delle applicazioni su piattaforme cloud, ottimizzazione delle performance e scalabilità.</li>
-                        </ul>
-                    </li>
-                </ol>
+                <section className="text-white mt-3">
+                    <ol>
+                        <li>
+                            <strong>HTML, CSS & JS:</strong>
+                            <ul>
+                                <li><strong>HTML:</strong> Struttura semantica delle pagine web.</li>
+                                <li><strong>CSS:</strong> Stile e layout delle pagine, adattamento responsivo.</li>
+                                <li><strong>JavaScript:</strong> Interattività e dinamismo sul lato client.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <strong>React:</strong>
+                            <ul>
+                                <li>Libreria JavaScript per la costruzione di interfacce utente dinamiche e componenti riutilizzabili.</li>
+                                <li>Gestione dello stato dell'applicazione con facilità, attraverso componenti e hook.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <strong>Node:</strong>
+                            <ul>
+                                <li>Ambiente di runtime JavaScript lato server, che permette l'esecuzione di JavaScript al di fuori del browser.</li>
+                                <li>Creazione di applicazioni server-side con efficienza e scalabilità.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <strong>Express:</strong>
+                            <ul>
+                                <li>Framework minimalista per Node.js, utilizzato per creare API e gestire routing e middleware.</li>
+                                <li>Semplificazione della gestione delle richieste HTTP, autenticazione e gestione delle sessioni.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <strong>MongoDB:</strong>
+                            <ul>
+                                <li>Database NoSQL orientato ai documenti, che consente di memorizzare dati in formato JSON-like.</li>
+                                <li>Flessibilità nella gestione di dati non strutturati e scalabilità per grandi volumi di dati.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <strong>Connecting & Deploying:</strong>
+                            <ul>
+                                <li><strong>Connecting:</strong> Integrazione del front-end e back-end, assicurando un flusso di dati senza interruzioni tra server e client.</li>
+                                <li><strong>Deploying:</strong> Distribuzione delle applicazioni su piattaforme cloud, ottimizzazione delle performance e scalabilità.</li>
+                            </ul>
+                        </li>
+                    </ol>
+                </section>
             )}
         </div>
     );
--- src/components/Content/progetti/Progetti.jsx
+++ src/components/Content/progetti/Progetti.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import Carousel from 'react-bootstrap/Carousel';
 import './Progetti.css';
 
 import Capstone from '../../../assets/Screenshot-Capstone.png';
@@ -15,40 +14,40 @@ export default function Progetti() {
             <div className="row">
                 <div className="col-md-3">
                     <div className="card bg-dark">
-                        <img className="card-img-top" src={Capstone} alt="Screenshot Capstone" />
+                        <img className="card-img-top" src={Capstone} alt="Screenshot Capstone Barber Shop" />
                         <div className="card-body d-flex flex-column align-items-center">
                             <h4 className="card-title text-light">Capstone Barber Shop</h4>
-                            <p className="card-text text-ligh">Gestionale completo per barbieri sviluppato con lo stack MERN. Frontend responsive per la prenotazione intuitiva di servizi; backend sicuro con autenticazione a token per la gestione degli utenti e delle operazioni CRUD.</p>
-                            {/* <a href="/#" className="btn btn-dark text-ligh">Scopri di più</a> */}
+                            <p className="card-text text-light">Gestionale completo per barbieri sviluppato con lo stack MERN. Frontend responsive per la prenotazione intuitiva di servizi; backend sicuro con autenticazione a token per la gestione degli utenti e delle operazioni CRUD.</p>
+                            {/* <a href="/#" className="btn btn-dark text-light">Scopri di più</a> */}
                         </div>
                     </div>
                 </div>
                 <div className="col-md-3">
                     <div className="card bg-dark">
-                        <img className="card-img-top" src={LinkSmart} alt="Screenshot Barberlink" />
+                        <img className="card-img-top" src={LinkSmart} alt="Screenshot Barber Links" />
                         <div className="card-body d-flex flex-column align-items-center">
                             <h4 className="card-title text-light">Barber Links</h4>
-                            <p className="card-text text-ligh">Codice QR per accedere rapidamente alle recensioni e ai canali social di 'Vito Gallo Barber Shop</p>
-                            {/* <a href="/#" className="btn btn-dark text-ligh">Scopri di più</a> */}
+                            <p className="card-text text-light">Codice QR per accedere rapidamente alle recensioni e ai canali social di 'Vito Gallo Barber Shop'</p>
+                            {/* <a href="/#" className="btn btn-dark text-light">Scopri di più</a> */}
                         </div>
                     </div>
                 </div>
                 <div className="col-md-3">
                     <div className="card bg-dark">
-                        <img className="card-img-top" src={TodoList} alt="TodoList-app" />
+                        <img className="card-img-top" src={TodoList} alt="Screenshot TodoList app" />
                         <div className="card-body d-flex flex-column align-items-center">
                             <h4 className="card-title text-light">TodoList-app</h4>
-                            <p className="card-text text-ligh">Interfaccia di un'applicazione ToDo List minimalista, con funzionalità per gestire e aggiungere compiti.</p>
-                            {/* <a href="/#" className="btn btn-dark text-ligh">Scopri di più</a> */}
+                            <p className="card-text text-light">Interfaccia di un'applicazione ToDo List minimalista, con funzionalità per gestire e aggiungere compiti.</p>
+                            {/* <a href="/#" className="btn btn-dark text-light">Scopri di più</a> */}
                         </div>
                     </div>
                 </div>
                 <div className="col-md-3">
                     <div className="card bg-dark">
-                        <img className="card-img-top" src={ComingSoon} alt="" />
+                        <img className="card-img-top" src={ComingSoon} alt="Coming Soon" />
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     );
 }
