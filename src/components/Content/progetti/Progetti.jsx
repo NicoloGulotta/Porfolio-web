ed57dcfe4d6bfb3a826e613fe53a9f60ec22b5d9
Nicolò Gulotta <gulottanico1998@gmail.com>
2024-09-19 20:36:03 +0200

    Save

--- src/components/Content/progetti/Progetti.jsx
+++ src/components/Content/progetti/Progetti.jsx
@@ -6,13 +6,15 @@ import Capstone from '../../../assets/Screenshot-Capstone.png';
 import LinkSmart from '../../../assets/Screenshot BarberLink-Smartphone.jpg';
 import ComingSoon from '../../../assets/images.png';
 import TodoList from '../../../assets/Screenshot 2024-09-05 211849.png';
+import Epibooks from '../../../assets/Screenshot 2024-09-19 182247.png'
+import NetflixClone from '../../../assets/Netflix-clone.png'
 
 export default function Progetti() {
     return (
         <div className="container-pro">
             <h2 className='text-light'>Progetti</h2>
             <div className="row">
-                <div className="col-md-3">
+                <div className="col ">
                     <div className="card bg-dark">
                         <img className="card-img-top" src={Capstone} alt="Screenshot Capstone Barber Shop" />
                         <div className="card-body d-flex flex-column align-items-center">
@@ -22,7 +24,7 @@ export default function Progetti() {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-3">
+                <div className="col m-3">
                     <div className="card bg-dark">
                         <img className="card-img-top" src={LinkSmart} alt="Screenshot Barber Links" />
                         <div className="card-body d-flex flex-column align-items-center">
@@ -32,9 +34,9 @@ export default function Progetti() {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-3">
+                <div className="col m-3">
                     <div className="card bg-dark">
-                        <img className="card-img-top" src={TodoList} alt="Screenshot TodoList app" />
+                        <img className="card-img-top" src={TodoList} alt="Screenshot app" />
                         <div className="card-body d-flex flex-column align-items-center">
                             <h4 className="card-title text-light">TodoList-app</h4>
                             <p className="card-text text-light">Interfaccia di un'applicazione ToDo List minimalista, con funzionalità per gestire e aggiungere compiti.</p>
@@ -42,7 +44,27 @@ export default function Progetti() {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-3">
+                <div className="col m-3">
+                    <div className="card bg-dark">
+                        <img className="card-img-top" src={Epibooks} alt="Screenshot app" />
+                        <div className="card-body d-flex flex-column align-items-center">
+                            <h4 className="card-title text-light">EpiBooks</h4>
+                            <p className="card-text text-light">EpiBooks è una piattaforma per scoprire libri, con dettagli visivi e recensioni personalizzate. Permette di cercare titoli, leggere riassunti e lasciare valutazioni in modo semplice e veloce.</p>
+                            {/* <a href="/#" className="btn btn-dark text-light">Scopri di più</a> */}
+                        </div>
+                    </div>
+                    <div className="col m-3">
+                        <div className="card bg-dark">
+                            <img className="card-img-top" src={NetflixClone} alt="Screenshot TodoList app" />
+                            <div className="card-body d-flex flex-column align-items-center">
+                                <h4 className="card-title text-light">NetClone</h4>
+                                <p className="card-text text-light">Ho creato un'interfaccia simile a Netflix con un sistema multi-carousel, che permette di navigare tra categorie di contenuti in modo fluido e responsive, riproducendo l'esperienza utente della piattaforma originale</p>
+                                {/* <a href="/#" className="btn btn-dark text-light">Scopri di più</a> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col m-3">
                     <div className="card bg-dark">
                         <img className="card-img-top" src={ComingSoon} alt="Coming Soon" />
                     </div>
