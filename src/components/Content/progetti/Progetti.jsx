8980a9e110ef0171eab82672ad31d46868abed2a
Nicolò Gulotta <gulottanico1998@gmail.com>
2024-09-23 17:46:47 +0200

    SAVE

--- src/components/Content/progetti/Progetti.jsx
+++ src/components/Content/progetti/Progetti.jsx
@@ -6,15 +6,15 @@ import Capstone from '../../../assets/Screenshot-Capstone.png';
 import LinkSmart from '../../../assets/Screenshot BarberLink-Smartphone.jpg';
 import ComingSoon from '../../../assets/images.png';
 import TodoList from '../../../assets/Screenshot 2024-09-05 211849.png';
-import Epibooks from '../../../assets/Screenshot 2024-09-19 182247.png'
-import NetflixClone from '../../../assets/Netflix-clone.png'
+import Epibooks from '../../../assets/Screenshot 2024-09-19 182247.png';
+import NetflixClone from '../../../assets/Netflix-clone.png';
 
 export default function Progetti() {
     return (
         <div className="container-pro">
-            <h2 className='text-light'>Progetti</h2>
+            <h2 className="text-light">Progetti</h2>
             <div className="row">
-                <div className="col ">
+                <div className="col-md-4 col-sm-6">
                     <div className="card bg-dark">
                         <img className="card-img-top" src={Capstone} alt="Screenshot Capstone Barber Shop" />
                         <div className="card-body d-flex flex-column align-items-center">
@@ -24,7 +24,7 @@ export default function Progetti() {
                         </div>
                     </div>
                 </div>
-                <div className="col m-3">
+                <div className="col-md-4 col-sm-6">
                     <div className="card bg-dark">
                         <img className="card-img-top" src={LinkSmart} alt="Screenshot Barber Links" />
                         <div className="card-body d-flex flex-column align-items-center">
@@ -34,7 +34,7 @@ export default function Progetti() {
                         </div>
                     </div>
                 </div>
-                <div className="col m-3">
+                <div className="col-md-4 col-sm-6">
                     <div className="card bg-dark">
                         <img className="card-img-top" src={TodoList} alt="Screenshot app" />
                         <div className="card-body d-flex flex-column align-items-center">
@@ -44,7 +44,7 @@ export default function Progetti() {
                         </div>
                     </div>
                 </div>
-                <div className="col m-3">
+                <div className="col-md-4 col-sm-6">
                     <div className="card bg-dark">
                         <img className="card-img-top" src={Epibooks} alt="Screenshot app" />
                         <div className="card-body d-flex flex-column align-items-center">
@@ -53,18 +53,18 @@ export default function Progetti() {
                             {/* <a href="/#" className="btn btn-dark text-light">Scopri di più</a> */}
                         </div>
                     </div>
-                    <div className="col m-3">
-                        <div className="card bg-dark">
-                            <img className="card-img-top" src={NetflixClone} alt="Screenshot TodoList app" />
-                            <div className="card-body d-flex flex-column align-items-center">
-                                <h4 className="card-title text-light">NetClone</h4>
-                                <p className="card-text text-light">Ho creato un'interfaccia simile a Netflix con un sistema multi-carousel, che permette di navigare tra categorie di contenuti in modo fluido e responsive, riproducendo l'esperienza utente della piattaforma originale</p>
-                                {/* <a href="/#" className="btn btn-dark text-light">Scopri di più</a> */}
-                            </div>
+                </div>
+                <div className="col-md-4 col-sm-6">
+                    <div className="card bg-dark">
+                        <img className="card-img-top" src={NetflixClone} alt="Screenshot app" />
+                        <div className="card-body d-flex flex-column align-items-center">
+                            <h4 className="card-title text-light">NetClone</h4>
+                            <p className="card-text text-light">Ho creato un'interfaccia simile a Netflix con un sistema multi-carousel, che permette di navigare tra categorie di contenuti in modo fluido e responsive, riproducendo l'esperienza utente della piattaforma originale</p>
+                            {/* <a href="/#" className="btn btn-dark text-light">Scopri di più</a> */}
                         </div>
                     </div>
                 </div>
-                <div className="col m-3">
+                <div className="col-md-4 col-sm-6">
                     <div className="card bg-dark">
                         <img className="card-img-top" src={ComingSoon} alt="Coming Soon" />
                     </div>
