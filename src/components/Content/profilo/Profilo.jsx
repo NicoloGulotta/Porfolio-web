d400dda666bcbbaa9ead424c267a7e7303b1b389
Nicolò Gulotta <gulottanico1998@gmail.com>
2024-09-23 19:44:32 +0200

    Save

--- src/components/Content/profilo/Profilo.jsx
+++ src/components/Content/profilo/Profilo.jsx
@@ -7,7 +7,7 @@ const Profilo = () => {
     return (
         <Container className="profilo d-flex align-items-center p-3">
             <img src={foto} alt="Nicolò Gulotta" className="profile-image" />
-            <div className="text-part ms-4">
+            <div className="profile-text ms-4">
                 <span>WEB DEVELOPER FULL-STACKS <strong>M.E.R.N.</strong> </span>
                 <p>Professionista versatile con una passione predominante per lo sviluppo web, con particolare esperienza nell'uso dello stack MERN (MongoDB, Express.js, React, Node.js). Ho maturato competenze nel creare soluzioni web dinamiche e scalabili, sfruttando le potenzialità di questa tecnologia per sviluppare applicazioni moderne e reattive.
                     Oltre alla mia esperienza nella ristorazione e logistica, che ha affinato le mie capacità di problem-solving e di lavoro in team, il mio vero interesse risiede nello sviluppo web. Sono continuamente impegnato nell'apprendimento di nuove tecnologie e metodologie,
