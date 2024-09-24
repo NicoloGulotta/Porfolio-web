f43f5c226327dba4fcec1f2ce91c0b17a6da42b0
Nicolò Gulotta <gulottanico1998@gmail.com>
2024-09-24 19:01:35 +0200

    Save

--- src/components/Content/profilo/Profilo.jsx
+++ src/components/Content/profilo/Profilo.jsx
@@ -7,8 +7,8 @@ const Profilo = () => {
     return (
         <Container className="profilo d-flex align-items-center p-3">
             <img src={foto} alt="Nicolò Gulotta" className="profile-image" />
-            <div className="profile-text ms-4">
-                <span>WEB DEVELOPER FULL-STACKS <strong>M.E.R.N.</strong> </span>
+            <div className="profile-text m-4 ">
+                <h3 className='text-white m-4'>WEB DEVELOPER FULL-STACKS <strong>M.E.R.N.</strong> </h3>
                 <p>Professionista versatile con una passione predominante per lo sviluppo web, con particolare esperienza nell'uso dello stack MERN (MongoDB, Express.js, React, Node.js). Ho maturato competenze nel creare soluzioni web dinamiche e scalabili, sfruttando le potenzialità di questa tecnologia per sviluppare applicazioni moderne e reattive.
                     Oltre alla mia esperienza nella ristorazione e logistica, che ha affinato le mie capacità di problem-solving e di lavoro in team, il mio vero interesse risiede nello sviluppo web. Sono continuamente impegnato nell'apprendimento di nuove tecnologie e metodologie,
                     con un focus specifico sull'ecosistema JavaScript, per migliorare le mie competenze tecniche e creare progetti web di alta qualità.</p>
--- src/components/Content/progetti/Progetti.jsx
+++ src/components/Content/progetti/Progetti.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Progetti.css';
 
@@ -9,66 +9,62 @@ import TodoList from '../../../assets/Screenshot 2024-09-05 211849.png';
 import Epibooks from '../../../assets/Screenshot 2024-09-19 182247.png';
 import NetflixClone from '../../../assets/Netflix-clone.png';
 
+const projects = [
+    {
+        img: Capstone,
+        title: "Capstone Barber Shop",
+        description: "Gestionale completo per barbieri sviluppato con lo stack MERN. Frontend responsive per la prenotazione intuitiva di servizi; backend sicuro con autenticazione a token per la gestione degli utenti e delle operazioni CRUD.",
+    },
+    {
+        img: LinkSmart,
+        title: "Barber Links",
+        description: "Codice QR per accedere rapidamente alle recensioni e ai canali social di 'Vito Gallo Barber Shop'",
+    },
+    {
+        img: TodoList,
+        title: "TodoList-app",
+        description: "Interfaccia di un'applicazione ToDo List minimalista, con funzionalità per gestire e aggiungere compiti.",
+    },
+    {
+        img: Epibooks,
+        title: "EpiBooks",
+        description: "EpiBooks è una piattaforma per scoprire libri, con dettagli visivi e recensioni personalizzate. Permette di cercare titoli, leggere riassunti e lasciare valutazioni in modo semplice e veloce.",
+    },
+    {
+        img: NetflixClone,
+        title: "NetClone",
+        description: "Ho creato un'interfaccia simile a Netflix con un sistema multi-carousel, che permette di navigare tra categorie di contenuti in modo fluido e responsive.",
+    },
+    { img: ComingSoon, title: "...", description: "" },
+];
+
 export default function Progetti() {
+    const [expandedCard, setExpandedCard] = useState(null);
+
+    const toggleExpand = (cardIndex) => {
+        setExpandedCard(prev => (prev === cardIndex ? null : cardIndex)); // Utilizza il valore precedente per semplificare
+    };
+
     return (
-        <div className="container-prog">
-            <h2 className="text-light">Progetti</h2>
+        <div className="container">
+            <h2 className="text-light mb-5">Progetti</h2>
             <div className="row">
-                <div className="col-md-4 col-sm-6">
-                    <div className="card bg-dark">
-                        <img className="card-img-top" src={Capstone} alt="Screenshot Capstone Barber Shop" />
-                        <div className="card-body d-flex flex-column align-items-center">
-                            <h4 className="card-title text-light">Capstone Barber Shop</h4>
-                            <p className="card-text text-light">Gestionale completo per barbieri sviluppato con lo stack MERN. Frontend responsive per la prenotazione intuitiva di servizi; backend sicuro con autenticazione a token per la gestione degli utenti e delle operazioni CRUD.</p>
-                            {/* <a href="/#" className="btn btn-dark text-light">Git Hub</a> */}
+                {projects.map((project, index) => (
+                    <div
+                        key={index}
+                        className={`col-md-4 col-sm-6 ${expandedCard === index ? ' expanded' : 'col-md-4 col-sm-6'}`}
+                        onClick={() => toggleExpand(index)}
+                        style={{ transition: 'transform 0.3s ease-in-out' }} // Aggiunta transizione generale
+                    >
+                        <div className="card bg-dark">
+                            <img className="card-image" src={project.img} alt={project.title} />
+                            <div className="card-body d-flex flex-column align-items-center">
+                                <h4 className="card-title text-light">{project.title}</h4>
+                                {project.description && <p className="card-text text-light">{project.description}</p>}
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-md-4 col-sm-6">
-                    <div className="card bg-dark">
-                        <img className="card-img-top" src={LinkSmart} alt="Screenshot Barber Links" />
-                        <div className="card-body d-flex flex-column align-items-center">
-                            <h4 className="card-title text-light">Barber Links</h4>
-                            <p className="card-text text-light">Codice QR per accedere rapidamente alle recensioni e ai canali social di 'Vito Gallo Barber Shop'</p>
-                            {/* <a href="/#" className="btn btn-dark text-light">Git Hub</a> */}
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4 col-sm-6">
-                    <div className="card bg-dark">
-                        <img className="card-img-top" src={TodoList} alt="Screenshot app" />
-                        <div className="card-body d-flex flex-column align-items-center">
-                            <h4 className="card-title text-light">TodoList-app</h4>
-                            <p className="card-text text-light">Interfaccia di un'applicazione ToDo List minimalista, con funzionalità per gestire e aggiungere compiti.</p>
-                            {/* <a href="/#" className="btn btn-dark text-light">Git Hub</a> */}
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4 col-sm-6">
-                    <div className="card bg-dark">
-                        <img className="card-img-top" src={Epibooks} alt="Screenshot app" />
-                        <div className="card-body d-flex flex-column align-items-center">
-                            <h4 className="card-title text-light">EpiBooks</h4>
-                            <p className="card-text text-light">EpiBooks è una piattaforma per scoprire libri, con dettagli visivi e recensioni personalizzate. Permette di cercare titoli, leggere riassunti e lasciare valutazioni in modo semplice e veloce.</p>
-                            {/* <a href="/#" className="btn btn-dark text-light">Git Hub</a> */}
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4 col-sm-6">
-                    <div className="card bg-dark">
-                        <img className="card-img-top" src={NetflixClone} alt="Screenshot app" />
-                        <div className="card-body d-flex flex-column align-items-center">
-                            <h4 className="card-title text-light">NetClone</h4>
-                            <p className="card-text text-light">Ho creato un'interfaccia simile a Netflix con un sistema multi-carousel, che permette di navigare tra categorie di contenuti in modo fluido e responsive, riproducendo l'esperienza utente della piattaforma originale</p>
-                            {/* <a href="/#" className="btn btn-dark text-light">Git Hub</a> */}
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4 col-sm-6">
-                    <div className="card bg-dark d-flex justify-content-center">
-                        <img className="card-img-top" src={ComingSoon} alt="Coming Soon" />
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     );
